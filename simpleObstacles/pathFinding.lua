9944170c62be6cd3a7ab6ccbf7c5699ec8e3c7e9
nathdwekULB <nathdwek@ulb.ac.be>
2013-11-08 13:30:09 +0100

    started pathfinding

---
+++ simpleObstacles/pathFinding.lua
@@ -0,0 +1,15 @@
+function newPath(path)
+   local cleanPart=path.cleanPart
+   local dirtyPart=path.dirtyPart
+   local ramify=function()
+      if dirtyPart is clean
+         cleanPart=cleanPart+dirtyPart
+      else
+         find the corners
+
+
+
+
+
+   end
+end
